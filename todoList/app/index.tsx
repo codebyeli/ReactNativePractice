0d460729ea6ef6f60b3eb9232c0a3b2c7a0b7bb5
codebyeli <hectorencarnacion2002@hotmail.com>
2025-09-28 03:51:10 -0400

    todoList completed creation functionality

--- todoList/app/index.tsx
+++ todoList/app/index.tsx
@@ -1,28 +1,48 @@
 import { router } from "expo-router";
-import React from "react";
-import { Text, StyleSheet, FlatList, Button } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, FlatList, Button, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import TextBox from "@/components/textBox";
 
-const TaskList = () => {
-  return (
-    <SafeAreaView>
-      <FlatList
-        data={[{ key: "Data" }]}
-        renderItem={({ item }) => <Text>{item.key}</Text>}
-      />
-      <Button
-        title="New task"
-        onPress={() => router.push("/createTask")}
-      />
-    </SafeAreaView>
-  );
+type Tasks = {
+  id?: number;
+  name: string;
+  description: string;
+  completed: boolean;
+  reward?: string;
 };
 
 function Index() {
+  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState<Tasks[]>([]);
+  const [taskForm, setTaskForm] = useState({
+    name: '',
+    description: '',
+    reward: '',
+  });
+
+  function createTask() {
+    setTasks([...tasks, {
+      id: count,
+      name: taskForm.name,
+      description: taskForm.description,
+      completed: false,
+      reward: taskForm.reward,
+    }])
+    setCount(count + 1)
+  }
+
   return (
     <SafeAreaView style={styles.body}>
       <Text style={styles.titleText}>To do app</Text>
-      <TaskList />
+      <TextBox label='Name' placeholder='Task name' value={taskForm.name} onChange={(text) => setTaskForm({ ...taskForm, name: text })} />
+      <TextBox label='Description' placeholder='Task description' value={taskForm.description} onChange={(text) => setTaskForm({ ...taskForm, description: text })} />
+      <TextBox label='Reward' placeholder='Reward for completing task' value={taskForm.reward} onChange={(text) => setTaskForm({ ...taskForm, reward: text })} />
+      <Button title='Save task' onPress={createTask} />
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </SafeAreaView>
   );
 }
--- todoList/components/textBox.tsx
+++ todoList/components/textBox.tsx
@@ -4,19 +4,26 @@ import React from "react";
 type TextBoxProps = {
   label: string;
   placeholder: string;
+  onChange?: (text: string) => void;
+  value?: string;
 };
 
 const TextBox = (props: TextBoxProps) => {
   return (
     <View>
       <Text>{props.label}</Text>
-      <TextInput placeholder={props.placeholder} />
+      <TextInput placeholder={props.placeholder} onChangeText={props.onChange} value={props.value}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    
+    labelText: {
+
+    },
+    input:{
+
+    }
 })
 
 export default TextBox;
