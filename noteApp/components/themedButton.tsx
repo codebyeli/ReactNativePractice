df39841ea03599d332736bf143224d632c4db990
codebyeli <nomadkillz007@gmail.com>
2026-02-04 18:38:32 -0400

    noteApp added transparent property to buttons

--- noteApp/components/themedButton.tsx
+++ noteApp/components/themedButton.tsx
@@ -5,15 +5,16 @@ import { Colors } from '@/constants/Colors'
 
 const ThemedButton = ({
     buttonText,
-    onPress
+    transparent,
+    onPress,
 }: any) => {
 
-        const colorScheme = useColorScheme() ?? 'light'
-        const theme = Colors[colorScheme]
-        
+    const colorScheme = useColorScheme() ?? 'light'
+    const theme = Colors[colorScheme]
+
     return (
         <View>
-            <Pressable style={[styles.button, { backgroundColor: theme.uiforeground }]} onPress={onPress}><ThemedText scheme='defaultSemiBold' type='ui' >{buttonText}</ThemedText></Pressable>
+            <Pressable style={[styles.button, transparent === true ? { backgroundColor: theme.uibackground } : { backgroundColor: theme.uiforeground }]} onPress={onPress}><ThemedText scheme='defaultSemiBold' type='ui' >{buttonText}</ThemedText></Pressable>
         </View>
     )
 }
@@ -23,9 +24,9 @@ export default ThemedButton
 const styles = StyleSheet.create({
     button: {
         padding: 15,
-        borderRadius:25,
+        borderRadius: 25,
         justifyContent: 'center',
         alignItems: 'center',
-        marginHorizontal:25
+        marginHorizontal: 25
     }
 })
